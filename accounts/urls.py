9bc9c44c33df1bf857258e5d40e138d36662be58
Rohan Parkes <rparkes@email.com>
2026-01-22 19:49:03 +1100

    Added Registration page.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 """Defines URL patters for Accounts."""
 
 from django.urls import path, include
+from . import views
 
 app_name = "accounts"  # ignore pylint: disable=invalid-name
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
+    path("register/", views.register, name="register"),
 ]
